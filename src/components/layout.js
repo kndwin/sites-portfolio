adedcb8dd3e576553992ad312348e2bd44f8f3ae
kndwin <tnkevino@gmail.com>
2020-07-15 22:46:02 +1000

    Added an AppBar, some icons, some cards

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,35 +1,65 @@
 import React from "react"
 import { Link } from "gatsby"
-import { Container, Typography } from "@material-ui/core"
+import { Typography, AppBar, Container,
+  Toolbar, IconButton, MenuItem, Menu} 
+  from "@material-ui/core"
+import MenuIcon from "@material-ui/icons/Menu"
+import TwitterIcon from "@material-ui/icons/Twitter"
+import GitHubIcon from "@material-ui/icons/GitHub"
+import EmailIcon from "@material-ui/icons/Email"
 import 'fontsource-roboto'
 import styles from "./layout.module.scss"
 
 export default function Layout ({ children }) {
+  const [anchorEl, setAnchorEl] = React.useState(null)
+  const handleClick = (e) => {
+    setAnchorEl(e.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
   return  (
-    <Container maxWidth="sm">
-      <div className={styles.centerRow}>
-        <Link className={styles.links} to="/">
-          <Typography variant="h4">kn</Typography>
-        </Link>
-        <Link className={styles.links}to="/projects/">
-          <Typography variant="h4">projects</Typography>
-        </Link>
-        <Link className={styles.links}to="/blog/">
-          <Typography variant="h4">blog</Typography>
-        </Link>
-      </div>
+    <Container maxWidth="xl">
+        <AppBar className={styles.AppBar}color="primary" position="sticky" >
+          <Toolbar variant="regular">
+            <IconButton onClick={handleClick} edge="start" color="inherit">
+              <MenuIcon /> 
+            </IconButton>
+            <Menu
+              anchorEl={anchorEl}
+              keepMounted
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={handleClose}>
+                <Link className={styles.links} to="/">
+                  <Typography >Home</Typography>
+                </Link>
+              </MenuItem >
+              <MenuItem onClick={handleClose}>
+                <Link className={styles.links} to="/projects/">
+                  <Typography >Projects</Typography>
+                </Link>
+              </MenuItem>
+              <MenuItem onClick={handleClose}>
+                <Link className={styles.links} to="/blog/">
+                  <Typography >Blog</Typography>
+                </Link>
+              </MenuItem>
+            </Menu>
+            <Typography variant="h6">Home</Typography>
+            <IconButton color="inherit">
+              <TwitterIcon /> 
+            </IconButton>
+            <IconButton color="inherit">
+              <GitHubIcon /> 
+            </IconButton>
+            <IconButton color="inherit">
+              <EmailIcon /> 
+            </IconButton>
+          </Toolbar>
+        </AppBar>
       {children}
-      <div>
-        <Link className={styles.footer}>
-          Twitter
-        </Link>
-        <Link className={styles.footer}>
-          Email
-        </Link>
-        <Link className={styles.footer}>
-          Github
-        </Link>
-      </div>
     </Container>
   )
 }
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,11 +1,37 @@
 import React from "react"
 import Layout from "../components/layout.js"
+import { Button,Card,CardContent,Typography,
+  CardActions,CardMedia } 
+  from "@material-ui/core"
 
 export default function Project() {
   return  (
     <Layout>
-      <h1>project</h1>
-      <p>Card layout</p>
+      <Typography variant="h3" gutterBottom>
+        Hello I am some text!
+      </Typography>
+      <Typography gutterBottom>
+        I'm trying my best!
+      </Typography>
+      <Card>
+        <CardContent>
+          <Typography color="textSecondary" gutterBottom>
+            Hello world!
+          </Typography>
+          <Typography variant="h5">
+            Material UI!
+          </Typography>
+        </CardContent>
+        <CardMedia 
+          component="img"
+          alt="project"
+          height="250"
+          src={require("./japan.jpg")}
+        />
+        <CardActions>
+          <Button>Project</Button>
+        </CardActions>
+      </Card>
     </Layout>
   )
 }
