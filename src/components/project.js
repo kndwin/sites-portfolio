64dbf5ba31763f8ad1e1f40a18ceea3803b29355
kndwin <tnkevino@gmail.com>
2020-08-09 01:38:57 +1000

    1. Filled out projects 2. Added SVG icons for the tags

--- src/components/project.js
+++ src/components/project.js
@@ -4,43 +4,158 @@ import styles from "./project.module.scss"
 export default function ProjectLinks (props) {
 
   // convert string to css module class name
-  let tagClass = (tag) => {
-    var cssModuleTag = "";
+  let tagInfo = (tag) => {
+    var tagInfo = {
+      cssModule: '',
+      link: '',
+      logo: ''
+    };
+
     switch (tag) {
       case 'node':
-        cssModuleTag = styles.node
+        tagInfo = {
+          cssModule: styles.node,
+          link: 'https://nodejs.org',
+          logo: 'https://simpleicons.org/icons/node-dot-js.svg'
+        }
         break;
       case 'heroku':
-        cssModuleTag = styles.heroku
+        tagInfo = {
+          cssModule: styles.heroku,
+          link: 'https://heroku.com',
+          logo: 'https://simpleicons.org/icons/heroku.svg'
+        }
+        break;
+      case 'mongodb':
+        tagInfo = {
+          cssModule: styles.mongodb,
+          link: 'https://mongodb.com',
+          logo: 'https://simpleicons.org/icons/mongodb.svg'
+        }
+        break;
+      case 'express':
+        tagInfo = {
+          cssModule: styles.express,
+          link: 'https://expressjs.com',
+        }
+        break;
+      case 'multer':
+        tagInfo = {
+          cssModule: styles.multer,
+          link: 'https://github.com/expressjs/multer',
+        }
+        break;
+      case 'react':
+        tagInfo = {
+          cssModule: styles.react,
+          link: 'https://reactjs.org',
+          logo: 'https://simpleicons.org/icons/react.svg'
+        }
+        break;
+      case 'bootstrap':
+        tagInfo = {
+          cssModule: styles.bootstrap,
+          link: 'https://getbootstrap.com',
+          logo: 'https://simpleicons.org/icons/bootstrap.svg'
+        }
+        break;
+      case 'codepen':
+        tagInfo = {
+          cssModule: styles.codepen,
+          link: 'https://codepen.io',
+          logo: 'https://simpleicons.org/icons/codepen.svg'
+        }
+        break;
+      case 'markdown':
+        tagInfo = {
+          cssModule: styles.markdown,
+          link: 'https://github.com/markedjs/marked',
+          logo: 'https://simpleicons.org/icons/markdown.svg'
+        }
+        break;
+      case 'sass':
+        tagInfo = {
+          cssModule: styles.sass,
+          link: 'https://sass-lang.com',
+          logo: 'https://simpleicons.org/icons/sass.svg'
+        }
+        break;
+      case 'redux':
+        tagInfo = {
+          cssModule: styles.redux,
+          link: 'https://redux.js.org',
+          logo: 'https://simpleicons.org/icons/redux.svg'
+        }
+        break;
+      case 'gatsby':
+        tagInfo = {
+          cssModule: styles.gatsby,
+          link: 'https://gatsbyjs.org',
+          logo: 'https://simpleicons.org/icons/gatsby.svg'
+        }
+        break;
+      case 'graphql':
+        tagInfo = {
+          cssModule: styles.graphql,
+          link: 'https://graphql.org',
+          logo: 'https://simpleicons.org/icons/graphql.svg'
+        }
+        break;
+      case 'netlify':
+        tagInfo = {
+          cssModule: styles.netlify,
+          link: 'https://netlify.com',
+          logo: 'https://simpleicons.org/icons/netlify.svg'
+        }
+        break;
+      case 'google-map':
+        tagInfo = {
+          cssModule: styles.google_map,
+          link: 'https://developers.google.com/maps/documentation',
+          logo: 'https://simpleicons.org/icons/googlemaps.svg'
+        }
         break;
       default: 
         break;
     }
-    return cssModuleTag;
+    return tagInfo;
   }
-
 
   return (
     <div>
-      <h4>{props.name}</h4>
-      <p>{props.shortDescription}</p>
-      <span>
-        <a href={props.linkToDemo}
-          className={styles.link}>
-          demo
-        </a>
-        &nbsp; (
-        <a href={props.linkToSourceCode}
-          className={styles.link}>
-          source code
-        </a>
-        )
-      </span>
-      {props.tags.map((tag) => 
-        <span className={`${tagClass(tag)} ${styles.tag}`}>
-          {tag}
+      <p>
+        <span className={styles.name}>
+          {props.name} -&nbsp;
         </span>
-      )}
+        {props.shortDescription}
+        <br />
+        <span>
+          <a href={props.linkToDemo}
+            className={styles.link}>
+            demo
+          </a>
+          &nbsp; (
+          <a href={props.linkToSourceCode}
+            className={styles.link}>
+            source code
+          </a>
+          )
+        </span>
+        <br />
+        <div className={styles.tag_group}>
+          {props.tags.map((tag) => 
+              <a href={tagInfo(tag).link}
+                className={styles.tag_link}>
+                <span className={`${tagInfo(tag).cssModule} 
+                  ${styles.tag}`}>
+                  {tag}
+                  <img src={tagInfo(tag).logo} 
+                    className={styles.logo}/>
+                </span>
+              </a>
+          )}
+        </div>
+      </p>
     </div>
   )
 }
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -6,30 +6,125 @@ export default function ProjectPage() {
   let projects = [
     {
       type: "backend",
-      name: "url shortner",
-      tags: ["node","heroku"],
-      linkToDemo: "example.com",
-      linkToSourceCode: "example.com/source",
-      shortDescription: "This project does this and that"
+      name: "💪 exercise tracker",
+      tags: ["node","express","heroku","mongodb"],
+      linkToDemo: "https://kndwin-fcc-exercise-tracker.herokuapp.com",
+      linkToSourceCode: "https://github.com/kndwin/fcc-project-exercisetracker",
+      shortDescription: "several REST API endpoints that tracks a user's exercise"
     },
     {
       type: "backend",
-      name: "file metadata",
-      tags: ["node","heroku"],
-      linkToDemo: "example.com",
-      linkToSourceCode: "example.com/source",
-      shortDescription: "This project does this and that"
+      name: "📏 url shortener",
+      tags: ["node","express","heroku","mongodb"],
+      linkToDemo: "https://kndwin-fcc-urlshortener.herokuapp.com",
+      linkToSourceCode: "https://github.com/kndwin/fcc-project-urlshortener",
+      shortDescription: "returns an endpoint that redirects to given URL"
+    },
+    {
+      type: "backend",
+      name: "📂 file metadata",
+      tags: ["node","express","heroku","multer"],
+      linkToDemo: "https://kndwin-fcc-filemetadata.herokuapp.com",
+      linkToSourceCode: "https://github.com/kndwin/fcc-project-filemetadata",
+      shortDescription: "returns metadata about an uploaded file"
+    },
+    {
+      type: "backend",
+      name: "⏱️ timestamp",
+      tags: ["node","express","heroku"],
+      linkToDemo: "https://kndwin-fcc-timestamp.herokuapp.com",
+      linkToSourceCode: "https://github.com/kndwin/fcc-project-timestamp",
+      shortDescription: "returns the date in ISO-8601 format"
+    },
+    {
+      type: "backend",
+      name: "🗣️ header parser",
+      tags: ["node","express","heroku"],
+      linkToDemo: "https://kndwin-fcc-headerparser.herokuapp.com",
+      linkToSourceCode: "https://github.com/kndwin/fcc-project-headerparser",
+      shortDescription: "returns header information of a GET request"
+    },
+    {
+      type: "frontend",
+      name: "💬 random quote generator",
+      tags: ["react","bootstrap","codepen"],
+      linkToDemo: "https://cdpn.io/kndwin/debug/GRgEEaB/wQMPozXQqYBk" ,
+      linkToSourceCode: "https://codepen.io/kndwin/pen/GRgEEaB",
+      shortDescription: "generates a random Simpsons quote"
+    },
+    {
+      type: "frontend",
+      name: "markdown previewer",
+      tags: ["react","markdown","codepen"],
+      linkToDemo: "https://cdpn.io/kndwin/debug/bGNRKBB/yPMJjKwpdoEM" ,
+      linkToSourceCode: "https://codepen.io/kndwin/details/bGNRKBB",
+      shortDescription: "converts raw text to html and previews it side by side"
+    },
+    {
+      type: "frontend",
+      name: "drumpad",
+      tags: ["react","sass","codepen"],
+      linkToDemo: "https://cdpn.io/kndwin/debug/rNawrKa/LQkExyGQaOVA" ,
+      linkToSourceCode: "https://codepen.io/kndwin/pen/rNawrKa",
+      shortDescription: "buttons generate sounds like a pad"
+    },
+    {
+      type: "frontend",
+      name: "pomodoro timer",
+      tags: ["react","sass","redux","codepen"],
+      linkToDemo: "https://cdpn.io/kndwin/debug/abOdKEz/LQMExyGQjPgk",
+      linkToSourceCode: "https://codepen.io/kndwin/pen/ab0dKEz",
+      shortDescription: "(unstylised) simple timer built to experiment react with redux"
+    },
+    {
+      type: "frontend",
+      name: "calculator",
+      tags: ["react","sass","codepen"],
+      linkToDemo: "https://cdpn.io/kndwin/debug/rNawrKa/LQkExyGQaOVA" ,
+      linkToSourceCode: "https://codepen.io/kndwin/pen/rNawrKa",
+      shortDescription: "(unstylised) simple calculator app built to experiment with react components"
+    },
+    {
+      type: "other",
+      name: "portfolio",
+      tags: ["gatsby","graphql","netlify"],
+      linkToDemo: "https://kndwin.dev",
+      linkToSourceCode: "https://github.com/kndwin/gatsby-portfolio",
+      shortDescription: "a gatsby generate site to showcase my projects"
+    },
+    {
+      type: "other",
+      name: "de-coco",
+      tags: ["react","google-map","netlify"],
+      linkToDemo: "https://de-coco.com.au",
+      linkToSourceCode: "https://github.com/kndwin/site-decoco-react-netlify",
+      shortDescription: "simple SPA site that for a local furniture business"
     },
   ]
+
+  let displayProject = (projectType) => {
+    return (
+      <div>
+        <h1>{projectType}</h1>
+        {projects
+          .filter((project) => project.type === projectType)
+          .map((project) => 
+            <ProjectLinks 
+              name={project.name} 
+              tags={project.tags}
+              linkToSourceCode={project.linkToSourceCode}
+              linkToDemo={project.linkToDemo}
+              shortDescription={project.shortDescription}/>)
+        }
+      </div>
+      )
+  }
+
   return  (
     <Layout>
-      {projects.map((project) => 
-        <ProjectLinks name={project.name} 
-          tags={project.tags}
-          linkToSourceCode={project.linkToSourceCode}
-          linkToDemo={project.linkToDemo}
-          shortDescription={project.shortDescription}/>
-      )}
+      {displayProject("backend")}
+      {displayProject("frontend")}
+      {displayProject("other")}
     </Layout>
   )
 }
